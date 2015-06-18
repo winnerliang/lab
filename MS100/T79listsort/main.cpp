66be571e010d32fd8488409dd680903b9ac252b3
yunlliang <yunlliang@localhost.localdomin>
2015-06-18 09:49:50 +0800

    2015.6.18

--- MS100/T79listsort/main.cpp
+++ MS100/T79listsort/main.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
+#include "mylist.h"
 
 using namespace std;
 
+void listsort(MyList * header)
+{
+    MyList * prev = header, * curt = header->next;
+    MyList * head = header;
+
+    for( ; curt!=NULL ; )
+    {
+        MyList *pcnt = head ;
+        for(; pcnt != curt; pcnt = pcnt->next)
+        {
+            if(head->value > curt->value)
+            {
+                head = curt;
+                prev->next = curt->next;
+                curt->next = pcnt;
+                curt = prev -> next;
+                break;
+            }
+            if(pcnt->next->value > curt->value)
+            {
+                //swap node
+                prev->next = curt->next;
+                curt->next = pcnt->next;
+                pcnt->next = curt;
+                curt = prev->next;
+                break;
+            }
+        }
+        if(pcnt == curt)
+        {
+            prev = prev->next;
+            curt =curt->next;
+        }
+    }
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    MyList * header = createlist(10);
+
+    displist(header);
+    cout<<endl;
+
+    listsort(header);
+
+    displist(header);
+    cout<<endl;
+
     return 0;
 }
