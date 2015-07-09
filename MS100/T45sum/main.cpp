b08cd703fef02e09bc41051c495ed95861f64f3c
yunlliang <yunlliang@localhost.localdomin>
2015-07-09 20:25:39 +0800

    20150709

--- MS100/T45sum/main.cpp
+++ MS100/T45sum/main.cpp
@@ -2,9 +2,51 @@
 
 using namespace std;
 
+void choosingsort(int pData[], int n)
+{
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(pData[i] > pData[j])
+            {
+                int t= pData[i];
+                pData[i]=pData[j];
+                pData[j]=t;
+            }
+        }
+    }
+}
+
 int partsum(int pData[], int n)
 {
+    //sorting
+    choosingsort(pData, n);
 
+    int allsum = 0;
+    for(int i=0; i<n; i++)
+        allsum += pData[i];
+
+    int Max = pData[n-1];
+    cout<<"MAX  = "<<Max<<endl;
+
+    int * pFlag = new int [n];
+
+    for(int m=2; m<= allsum/Max; m++)
+    {
+        if( allsum % m == 0)
+        {
+            //fill the condition
+            int segsum = allsum / m;
+            int cursum =0;
+            for(int i=0; i< n; i++)
+            {
+                if()
+            }
+        }
+    }
+
+    delete []pFlag;
 }
 
 int main()
